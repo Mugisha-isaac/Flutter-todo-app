31513c7130adbf9978f6e912a6bd7e776710a119
Mugisha-isaac <mugishaisaac2001@gmail>
2022-03-01 21:37:17 +0200

    creating todo card

---
+++ lib/widgets/todo_cards.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TodoCard extends StatelessWidget {
+  const TodoCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      child: Text('todo card'),
+    );
+  }
+}
